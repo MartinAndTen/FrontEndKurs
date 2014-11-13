5114474cbf9ff2af1a48daf9cdd35e1f6e3cd904
Martin Andersson-Tenninge <martin.andersson-tenninge@yh.nackademin.se>
2014-11-13 13:20:46 +0100

    flyttade på js-länkarna

---
+++ FrontEndKurs2014/LocalStorage/LocalStorage/Scripts/LocalStorage.js
@@ -0,0 +1,9 @@
+﻿function saveNameToLocalStorage() {
+    localStorage.lastEnteredName = document.getElementById("nameField").value;
+    document.getElementById("lastEnteredName").innerHTML = localStorage.lastEnteredName;
+    document.getElementById("lastEnteredNamePopup").innerHTML = localStorage.lastEnteredName + " has been saved to local storage";
+}
+
+$(document).ready(function LoadLocalStorage() {
+    document.getElementById("lastEnteredName").innerHTML = localStorage.lastEnteredName;
+})
