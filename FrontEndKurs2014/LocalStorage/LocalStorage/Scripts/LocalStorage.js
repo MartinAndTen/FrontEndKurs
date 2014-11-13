dc150ff1fd2b18d28c6cdb0ff73262cf08234374
Martin Andersson-Tenninge <martin.andersson-tenninge@yh.nackademin.se>
2014-11-13 15:31:00 +0100

    fortsatt labbande med mobil-sidan

--- FrontEndKurs2014/LocalStorage/LocalStorage/Scripts/LocalStorage.js
+++ FrontEndKurs2014/LocalStorage/LocalStorage/Scripts/LocalStorage.js
@@ -4,6 +4,15 @@
     document.getElementById("lastEnteredNamePopup").innerHTML = localStorage.lastEnteredName + " has been saved to local storage";
 }
 
-$(document).ready(function LoadLocalStorage() {
+$(document).on("pagecreate", "#StartPage", function LoadLocalStorage() {
     document.getElementById("lastEnteredName").innerHTML = localStorage.lastEnteredName;
 })
+
+$("#firstA").on("swipeleft", function () {
+    $(window.location.href = "#pageTwo");
+});
+
+
+$("#secondA").on("swiperight", function () {
+    $(window.location.href = "#StartPage");
+});
